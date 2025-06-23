67fd86e9611106eadb2c512a256ff46a68c69d3b
na10tran <nathanctran1225@gmail.com>
2025-06-23 01:07:57 -0700

    fixing graph to plot ma and mv over time

--- src/main_window.py
+++ src/main_window.py
@@ -449,8 +449,8 @@ class MainWindow(QWidget):
             self.clear_graph()            
             self.update_plot(serial=None)
             self.set_controls_enabled(False)
-            #self.clear_graph_button.setEnabled(False)
-            self.remove_running_button.setEnabled(False)
+            self.clear_graph_button.setEnabled(False)
+            #self.remove_running_button.setEnabled(False)
 
     def set_controls_enabled(self, enabled):
         """
